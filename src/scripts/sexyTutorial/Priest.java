9afa054e60b2a72d6dcb53aa54c3e3ae782f67ef
grandwizard420 <sluttmuffinz420@gmail.com>
2018-11-08 15:54:07 -0800

    finished up, still needs debugging...
    nullptr at the poll
    
    combat is really buggy
    
    sticks on toggle run after cook
    
    runs from rat cage to mining instructor to combat instructor

---
+++ src/scripts/sexyTutorial/Priest.java
@@ -0,0 +1,104 @@
+package scripts.sexyTutorial;
+
+import org.tribot.api.General;
+import org.tribot.api.util.abc.ABCUtil;
+import org.tribot.api2007.*;
+
+public class Priest extends Node {
+
+    Priest(ABCUtil a){
+        abc = a;
+    }//make all nodes share the same ABCUtil (My Java is a bit shaky, please correct me
+    // if this isn't right, or if there's a better way to do this!!!)
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Node specific variables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ABCUtil abc;
+
+    private final String intro = "<col=0000ff>Prayer</col><br>Follow the path to the chapel and enter it." +
+            "<br>Once inside talk to the monk. He'll tell you all about the Prayer skill.";
+
+    private final String prayerMenu = "<col=0000ff>Prayer menu</col><br>Click on the flashing icon " +
+            "to open the Prayer menu.";
+
+    private final String prayerExplain = "<col=0000ff>Prayer menu</col><br>Talk with Brother Brace " +
+            "and he'll tell you about prayers.";
+
+    private final String friends = "<col=0000ff>Friends and Ignore lists</col><br>You should now see another new " +
+            "icon. Click on the flashing face icon to open your friends and ignore lists.";
+
+    private final String friendsExplain = "<col=0000ff>Friends and Ignore lists</col><br>These two lists can be " +
+            "very helpful for keeping track of your friends or for blocking people you don't like. You can swap " +
+            "between the two lists using the button in the top right corner of the menu. Speak with Brother " +
+            "Brace to learn more.";
+
+    private final String leave = "<col=0000ff>Your final instructor!<br>You're almost finished on tutorial " +
+            "island. Pass through the door to find the path leading to your final instructor.";
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Tutorial Step Methods~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private void intro(){
+        Utils.walkToPriest(abc);
+        Utils.talkTo("Brother Brace");
+    }
+
+    private void prayerMenu(){
+        GameTab.open(GameTab.TABS.PRAYERS);
+    }
+
+    private void prayerExplain(){
+        Utils.walkToPriest(abc);
+        Utils.talkTo("Brother Brace");
+    }
+
+    private void friends(){
+        GameTab.open(GameTab.TABS.FRIENDS);
+    }
+
+    private void friendsExplain(){
+        Utils.walkToPriest(abc);
+        Utils.talkTo("Brother Brace");
+    }
+
+    private void leave(){
+        Utils.walkToWizard(abc);
+    }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Node framework~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    @Override
+    public void execute(){
+        if(Interfaces.get(263,1,0).getText().equals(intro)) {
+            System.out.println("Introducing Brother Brace!");
+            intro();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(prayerMenu)) {
+            System.out.println("Opening prayer menu");
+            prayerMenu();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(prayerExplain)) {
+            System.out.println("gettin the rundown on prayers!");
+            prayerExplain();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(friends)) {
+            System.out.println("Opening friends list");
+            friends();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(friendsExplain)) {
+            System.out.println("leaning wtf a friend is");
+            friendsExplain();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(leave)) {
+            System.out.println("Getting the fuck away from this child molester priest");
+            leave();
+        }
+
+        General.sleep(800, 1200);
+    }
+
+    @Override
+    public boolean validate(){
+        if(Game.getSetting(406) == 16 || Game.getSetting(406) == 17)//the setting for tutorial island progress
+            return true;
+        else
+            return false;
+    }
+}
---
+++ src/scripts/sexyTutorial/Wizard.java
@@ -0,0 +1,115 @@
+package scripts.sexyTutorial;
+
+import org.tribot.api.General;
+import org.tribot.api.util.abc.ABCUtil;
+import org.tribot.api2007.*;
+import org.tribot.api2007.types.*;
+
+public class Wizard extends Node {
+
+    Wizard(ABCUtil a){
+        abc = a;
+    }//make all nodes share the same ABCUtil (My Java is a bit shaky, please correct me
+    // if this isn't right, or if there's a better way to do this!!!)
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Node specific variables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ABCUtil abc;
+
+    private final String intro = "<col=0000ff>Your final instructor!</col><br>Follow the path to the wizard's house, " +
+            "where you will be shown how to cast spells. When you get there, just talk with the magic instructor.";
+
+    private final String openMagic = "<col=0000ff>Open up your final menu</col><br>Open up the magic interface by " +
+            "clicking on the flashing icon.";
+
+    private final String magic = "<col=0000ff>Magic</col><br>This is your magic interface. All of your spells " +
+            "are listed here. Talk to the instructor to learn more.";
+
+    private final String magicCast = "<col=0000ff>Magic casting</col><br>You now have some runes. All spells require " +
+            "runes to cast them. Look for the Wind Strike spell in your magic interface. Click on this spell to " +
+            "select it and then click on a chicken to cast it. Talk to the instructor if you need more runes.";
+
+    private final String toMainland = "<col=0000ff>To the mainland!</col><br>You're nearly finished with the " +
+            "tutorial. All you need to do now is move on to the mainland. Just speak with the magic instructor " +
+            "and he'll teleport you to Lumbridge.";
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Tutorial Step Methods~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private void intro() {
+        Utils.walkToWizard(abc);
+        Utils.talkTo("Magic Instructor");
+    }
+
+    private void openMagic(){
+        GameTab.open(GameTab.TABS.MAGIC);
+    }
+
+    private void magic(){
+        Utils.walkToWizard(abc);
+        Utils.talkTo("Magic Instructor");
+    }
+
+    private void magicCast(){
+        Utils.walkToWizard(abc);
+        RSNPC[] chicken = NPCs.findNearest("Chicken");
+        if(chicken.length < 1)
+            return;
+        Magic.selectSpell("Wind Strike");
+        chicken[0].click("Cast Wind Strike -> Chicken");
+    }
+
+    private void toMainland(){
+        Utils.walkToWizard(abc);
+        Utils.talkTo("Magic Instructor");
+        if(NPCChat.getOptions() == null)
+            return;
+        NPCChat.selectOption("Yes.", true);
+        General.sleep(1000, 2000);
+        NPCChat.getClickContinueInterface().click("Continue");
+        General.sleep(500, 2000);
+        NPCChat.selectOption("No. I'm not planning to do that.", true);
+        General.sleep(1000, 2000);
+        NPCChat.getClickContinueInterface().click("Continue");
+        General.sleep(1000, 2000);
+        NPCChat.getClickContinueInterface().click("Continue");
+        General.sleep(1000, 2000);
+        NPCChat.getClickContinueInterface().click("Continue");
+        General.sleep(1000, 2000);
+        NPCChat.getClickContinueInterface().click("Continue");
+        General.sleep(2000, 5000);
+    }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Node framework~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    @Override
+    public void execute(){
+        if(Interfaces.get(263,1,0).getText().equals(intro)) {
+            System.out.println("Getting magic intro");
+            intro();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(openMagic)) {
+            System.out.println("Opening magic tab");
+            openMagic();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(magic)) {
+            System.out.println("Getting the rundown on magic from this yung wiz!");
+            magic();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(magicCast)) {
+            System.out.println("Casting wind strike!");
+            magicCast();
+        }
+        else if(Interfaces.get(263,1,0).getText().equals(toMainland)) {
+            System.out.println("Travelling to mainland!");
+            toMainland();
+        }
+
+        General.sleep(800, 1200);
+    }
+
+    @Override
+    public boolean validate(){
+        if(Game.getSetting(406) >= 18)//the setting for tutorial island progress
+            return true;
+        else
+            return false;
+    }
+}
